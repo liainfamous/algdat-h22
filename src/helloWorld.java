e53cfda41a29c93f1763625977fc4690f7fa3a3e
liainfamous <s346482@oslomet.no>
2022-08-24 11:06:41 +0200

    Uke 34

---
+++ src/helloWorld.java
@@ -0,0 +1,10 @@
+public class helloWorld {
+    public static void main(String [] args) {
+        System.out.println("Hello world");
+
+        System.out.println("Teller ned til ti");
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
